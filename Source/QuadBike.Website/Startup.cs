4691acbfa7e39e6f7b4ccf3322a816d6a83262a1
NikitosV <Niktos.V@tut.by>
2019-03-27 22:34:57 +0300

    fix structure

--- Source/QuadBike.Website/Startup.cs
+++ Source/QuadBike.Website/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace QuadBike
+namespace QuadBike.Website
 {
     public class Startup
     {
