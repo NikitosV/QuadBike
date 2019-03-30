cda83838a9b4a2e4cdc44e28af8e1f1ecbc9afed
NikitosV <Niktos.V@tut.by>
2019-03-30 16:18:15 +0300

    Up version of ASP.NET Core to 2.2

--- Source/QuadBike.Website/Startup.cs
+++ Source/QuadBike.Website/Startup.cs
@@ -6,10 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using QuadBike.DataProvider.EF;
 
 namespace QuadBike.Website
 {
@@ -25,9 +23,7 @@ namespace QuadBike.Website
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<QuadBikeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddMvc();/*.SetCompatibilityVersion(CompatibilityVersion.Version_2_1)*/
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
