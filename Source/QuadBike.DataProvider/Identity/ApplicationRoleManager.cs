51189ca289c208dea1a38c70044159ab6005ef38
NikitosV <Niktos.V@tut.by>
2019-04-03 08:24:04 +0300

    add folder Identiy

---
+++ Source/QuadBike.DataProvider/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadBike.DataProvider.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(RoleStore<ApplicationRole> store) : base(store)
+        {
+
+        }
+    }
+}
---
+++ Source/QuadBike.DataProvider/Identity/ApplicationUserManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using QuadBike.DataProvider.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadBike.DataProvider.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+        }
+    }
+}
