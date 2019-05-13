a1308e68d5e1727ecef104fdff900bb9cba75513
HomePC <Niktos.V@tut.by>
2019-05-13 15:43:43 +0300

    fix views in home

---
+++ Source/QuadBike.Model/ViewModel/Pagination/TripPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadBike.Model.ViewModel.Pagination
+{
+    public class TripPageViewModel
+    {
+        public int PageNumber { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public TripPageViewModel(int count, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageNumber > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageNumber < TotalPages);
+            }
+        }
+    }
+}
