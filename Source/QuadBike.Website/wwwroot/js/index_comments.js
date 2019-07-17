5e93b41d911d7dd6db0f792b5fc4d89831a3a6c4
HomePC <Niktos.V@tut.by>
2019-07-17 14:48:54 +0300

    comments

---
+++ Source/QuadBike.Website/wwwroot/js/index_comments.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    $("#addComment").click(function () {
+        var comment = $("#inputContentComment").val();
+        if (comment) {
+            var xhr = new XMLHttpRequest();
+
+            var json = JSON.stringify({
+                Content: comment
+            });
+
+            xhr.open("POST", '/Comment', true)
+            xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            xhr.send(json);
+        }
+    });
+
+});
