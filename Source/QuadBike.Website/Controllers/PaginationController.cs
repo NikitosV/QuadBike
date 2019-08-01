b4965dc8cca5a6bed2c68e7bc1d4c1deb4a5569a
NikitosV <Niktos.V@tut.by>
2019-08-01 11:54:37 +0300

    add test pagination func

---
+++ Source/QuadBike.Website/Controllers/PaginationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuadBike.Logic.Interfaces;
+using QuadBike.Model.Entities;
+
+namespace QuadBike.Website.Controllers
+{
+    public class PaginationController : Controller
+    {
+        private IBikeService _bikeService;
+        private ITripService _tripService;
+        private IUserManagerService _userManagerService;
+
+        public PaginationController(IBikeService bikeService, ITripService tripService, IUserManagerService userManagerService)
+        {
+            _bikeService = bikeService;
+            _tripService = tripService;
+            _userManagerService = userManagerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAllBikes()
+        {
+            //List<Bike> ObjEmp = new List<Bike>()
+            //{
+            //    new Bike()
+            //    {
+            //        Id = 1, Name = "TEST", MaxSpeed = 100, TypeEngine = "test", Power = 100, Fuel = 200, Description = "121", Price = 100
+            //    }
+            //};
+
+            var source = _bikeService.GetAllBikes();
+            //JsonResult categoryJson = new JsonResult(source);
+            return Json(source);
+        }
+    }
+}
